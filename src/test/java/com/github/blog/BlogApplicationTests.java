e293a6227712d6e98923baf99c1ee4e3ff75a627
João Medeiros <joao.pereira.118@ufrn.edu.br>
2024-01-25 11:17:58 -0300

    test: posts controller dependency injection

--- src/test/java/com/github/blog/BlogApplicationTests.java
+++ src/test/java/com/github/blog/BlogApplicationTests.java
@@ -1,13 +1,22 @@
 package com.github.blog;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.github.blog.controllers.PostsController;
 
 @SpringBootTest
 class BlogApplicationTests {
 
+	@Autowired
+	private PostsController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();	
 	}
 
 }
